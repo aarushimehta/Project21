fa61be54bf5869bb25bc6786bca7672660b76fe3
aarushimehta <aarushi2.mehta@gmail.com>
2020-12-02 18:50:34 -0800

    modified sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,43 @@
+var wall, thickness;
+var bullet, speed, weight;
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(1600,400);
+  speed = random(223,321);
+  weight = random(30,52);
+  thickness = random(22,83);
+  
+  bullet = createSprite(50, 200, 50, 5);
+  bullet.velocityX = speed;
+  bullet.shapeColor = "black";
+
+  wall = createSprite(1200,200,thickness,height/2);
+  wall.shapeColor = "blue";
 }
 
 function draw() {
-  background(255,255,255);  
+  background("pink"); 
+  if(hasCollided(bullet,wall)){
+
+    bullet.velocityX = 0;
+    var damage = 0.5 * weight * speed * speed/(thickness * thickness * thickness);
+
+    if(damage>10){
+      wall.shapeColor = "blue"
+    }
+    
+    if(damage<10){
+      wall.shapeColor = "yellow";
+    }
+  } 
   drawSprites();
+}
+
+function hasCollided(Lbullet, Lwall){
+  bulletRightEdge = Lbullet.x+Lbullet.width;
+  wallLeftEdge = Lwall.x;
+  if (bulletRightEdge>=wallLeftEdge){
+    return true
+  }
+  return false;
 }
